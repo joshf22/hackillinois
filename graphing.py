ed9d3659721623adb6bc7e5a8bca070c9f130836
justinbifeld <47907276+justinbifeld@users.noreply.github.com>
2019-02-23 17:28:49 -0600

    Add files via upload

--- graphing.py
+++ graphing.py
@@ -3,20 +3,29 @@ import plotly.plotly as py              # for sending things to plotly
 import plotly.tools as tls              # for mpl, config, etc.
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
+import datetime as dt
 
 from numpy import genfromtxt
-my_data = genfromtxt('hackillinois/GE.csv', delimiter=',', skip_header=1)
-
+my_data = genfromtxt('hackillinois/GE.csv', delimiter=',', skip_header=1, dtype=(int,float,float,float,float,float,float))
+print(my_data)
 columns_count = len(my_data[0])
 stock = [[],[],[],[],[],[],[]]
 
 
 for row in my_data: 
     for column, column_value in enumerate(row,0):
+        if column == 0:
+            column_value = str(column_value)
+            column_value = dt.datetime.strptime(column_value,"%Y%m%d").date()
         stock[column].append(column_value)
+        
+
+
+
+
+
 
 plt.plot(stock[0],stock[1],'ro')
-plt.axis([20190123,20190222,9,11])
 plt.show()
 
 print (stock)
