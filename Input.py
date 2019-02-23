7d8d995c32dc61806c18afd0f7c33651db2c372f
Kushnu <sc21@illinois.edu>
2019-02-23 17:02:29 -0600

    outputs dates

--- Input.py
+++ Input.py
@@ -1,18 +1,17 @@
 import csv
-import numpy
 stuff = input("Start year: ")
-startyear = int(stuff)
-while (startyear > 2020):
+Startyear = int(stuff)
+while (Startyear > 2020):
     print ("invalid year")
     stuff = input("Start year: ")
-    startyear = int(stuff)
+    Startyear = int(stuff)
 
 stuff = input("Start month: ")
-startmonth = int(stuff)
-while (startmonth > 12) or (startmonth < 1):
+Startmonth = int(stuff)
+while (Startmonth > 12) or (Startmonth < 1):
     print ("invalid month")
     stuff = input("Start month: ")
-    startmonth = int(stuff)
+    Startmonth = int(stuff)
 
 
 stuff = input("Start day: ")
@@ -24,30 +23,25 @@ while (startday > 31) or (startday < 1):
 
 stuff = input("End year: ")
 Endyear = int(stuff)
-while (Endyear > 2020) or (Endyear < 1000):
+while (Endyear > 2020) or (Endyear < 1000) or (Endyear < Startyear):
     print ("invalid Year \n")
     stuff = input("End Year: ")
-    startday = int(stuff)
+    Endyear = int(stuff)
 
 stuff = input("End month: ")
 Endmonth = int(stuff)
-while (endmonth > 12) or (endmonth < 1):
+while (Endmonth > 12) or (Endmonth < 1) or ((Endyear == Startyear) and (Endmonth < Startmonth)):
     print ("invalid month \n")
     stuff = input("End month: ")
-    startday = int(stuff)
+    Endmonth = int(stuff)
 
 stuff = input("End day: ")
-endday = int(stuff)
-while (endday > 31) or (endday < 1):
-    print ("invalid day \n")
+Endday = int(stuff)
+while (Endday > 31) or (Endday < 1) or ((Endyear == Startyear) and (Endmonth == Startmonth) and (Endday < startday)):
+    print ("invalid day")
     stuff = input("End day: ")
-    startday = int(stuff)
-
-if (startyear > 2020):
-    print ("invalid year")
+    Endday = int(stuff)
 
 
-
-
-
-print ("hello world")
+print ("Getting data from {year}-{month}-{day}" .format(year=Startyear, month=Startmonth, day=startday))
+print ("to {year}-{month}-{day}" .format(year=Endyear, month=Endmonth, day=Endday))
